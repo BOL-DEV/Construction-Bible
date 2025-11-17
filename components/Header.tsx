9a82e4912706faff444362e20c7a8c86b4fccbb2
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-11-17 21:40:57 +0100

    navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,23 +25,26 @@ function Header() {
   //   const {} = props;
 
   return (
-    <nav className="flex items-center justify-between p-4 border-b w-full bg-amber-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
+    <nav className="flex items-center justify-between p-4 px-40 border-b w-full bg-amber-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
       <Link href="/">
         <div className="flex items-center gap-3">
           <h1 className="bg-amber-600 border-0 rounded-lg pointer p-3">CB</h1>
           <div>
-            <h2>Construction Bible</h2>
+            <h2 className="text-xl font-bold">Construction Bible</h2>
             <p>Building Knowledge Base</p>
           </div>
         </div>
       </Link>
 
-      <div>
-        <h2>
-          <BiHeart /> Favorites
+      <div className="flex items-center gap-10">
+        <h2 className="flex items-center gap-2.5 hover:bg-gray-400/20 p-2 rounded-lg cursor-pointer">
+          <BiHeart /> <span>Favorites</span>
         </h2>
 
-        <BiSun onClick={handleTheme} />
+        <BiSun
+          className=" text-4xl hover:bg-gray-400/20 p-2 rounded-lg cursor-pointer"
+          onClick={handleTheme}
+        />
       </div>
     </nav>
   );
