ff5b3634857ca061cdf5c58c636edc59a5769805
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-11-18 17:03:30 +0100

    navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,41 +4,36 @@ import Link from "next/link";
 import React from "react";
 import { BiHeart, BiSun } from "react-icons/bi";
 
-// interface Props {}
+// interface Props {
+//   title: string;
+// }
 
 function Header() {
   const handleTheme = () => {
-    console.log("clicked");
-
     const html = document.documentElement;
 
     console.log(html);
 
-    if (html.classList.contains("dark")) {
-      html.classList.remove("dark");
-    } else {
-      html.classList.add("dark");
-    }
-    // html.classList.toggle("dark");
+    html.classList.toggle("dark");
   };
 
-  //   const {} = props;
+  // const { title } = props;
 
   return (
-    <nav className="flex items-center justify-between p-4 px-40 border-b w-full bg-amber-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
+    <nav className="flex items-center justify-between p-4 lg:px-40 border-b w-full bg-amber-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
       <Link href="/">
         <div className="flex items-center gap-3">
           <h1 className="bg-amber-600 border-0 rounded-lg pointer p-3">CB</h1>
           <div>
-            <h2 className="text-xl font-bold">Construction Bible</h2>
-            <p>Building Knowledge Base</p>
+            <h2 className="lg:text-xl text-md font-bold">Construction Bible</h2>
+            <p className="text-sm text-gray-400">Building Knowledge Base</p>
           </div>
         </div>
       </Link>
 
       <div className="flex items-center gap-10">
         <h2 className="flex items-center gap-2.5 hover:bg-gray-400/20 p-2 rounded-lg cursor-pointer">
-          <BiHeart /> <span>Favorites</span>
+          <BiHeart /> <span className="hidden lg:flex">Favorites</span>
         </h2>
 
         <BiSun
