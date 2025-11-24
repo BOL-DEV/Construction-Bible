2a255fc9ebe4c4997ec562269413d3c8c9e1f440
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-11-24 23:35:10 +0100

    Adding caterogies part

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,20 +1,70 @@
 import React from "react";
+import CategoriesCard from "./CategoriesCard";
+import { BsTools, BsBricks } from "react-icons/bs";
+import { LuBrickWall } from "react-icons/lu";
+import { GiCrane } from "react-icons/gi";
+import { FiBookOpen } from "react-icons/fi";
+import { LiaUserTieSolid } from "react-icons/lia";
 
 // interface Props {}
+
+const categoryData = [
+  {
+    title: "materials",
+    terms: "Concrete, Wood, Steel, and more",
+    image: <LuBrickWall />,
+  },
+  {
+    title: "tools",
+    terms: "Hand Tools, Power Tools, Measuring Tools, and more",
+    image: <BsTools />,
+  },
+  {
+    title: "machines",
+    terms: "Heavy equiment and machinery ",
+    image: <GiCrane />,
+  },
+  {
+    title: "techniques",
+    terms: "Construction Methods, Safety Practices, and more",
+    image: <FiBookOpen />,
+  },
+  {
+    title: "professionals",
+    terms: "Rolees and Responsibilities",
+    image: <LiaUserTieSolid />,
+  },
+  {
+    title: "building Elements",
+    terms: "foundations, walls, roofs, and more",
+    image: <BsBricks />,
+  },
+];
 
 function Categories() {
   // const {} = props
 
   return (
     <section>
-      <div className="flex flex-col items-center bg-amber-50 dark:bg-neutral-900 dark:text-gray-200 p-15 gap-4 ">
+      <div className="flex flex-col items-center bg-amber-50 dark:bg-neutral-900 dark:text-gray-200 lg:p-15 p-3 gap-4 ">
         <h1 className="text-3xl font-bold">Browse by Category</h1>
-        <p className="text-gray-400">
+        <p className="text-gray-400 text-center lg:text-lg md:text-md text-sm max-w-2xl">
           Explore construction knowledge organized by topic
         </p>
       </div>
 
-      <div></div>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 p-6 lg:px-40 bg-amber-50 dark:bg-neutral-900 dark:text-gray-200">
+        {categoryData.map((category) => (
+          <CategoriesCard
+            key={category.title}
+            title={
+              category.title.charAt(0).toUpperCase() + category.title.slice(1)
+            }
+            terms={category.terms}
+            image={category.image}
+          />
+        ))}
+      </div>
     </section>
   );
 }
---
+++ components/CategoriesCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  terms: string;
+  image?: React.ReactNode;
+}
+
+function CategoriesCard(Props: Props) {
+  const { title, terms, image } = Props;
+
+  const modifyedTitle = title.toLowerCase().replace(/\s+/g, "-");
+
+  return (
+    <Link href={`/categories/${modifyedTitle}`}>
+      <div
+        key={title}
+        className="group flex flex-col gap-3 border text-center border-gray-300 hover:border-amber-600 dark:border-neutral-700 rounded-lg p-14 hover:shadow-lg transition-shadow cursor-pointer bg-amber-50 dark:bg-neutral-800"
+      >
+        <div className="flex items-center justify-center text-3xl p-4 bg-gray-300 group-hover:bg-orange-400 dark:group-hover:text-black dark:bg-neutral-700  rounded-lg mx-auto">
+          {image}
+        </div>
+        <h2 className="text-xl font-semibold">{title}</h2>
+        <p className="text-gray-400">{terms}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default CategoriesCard;
